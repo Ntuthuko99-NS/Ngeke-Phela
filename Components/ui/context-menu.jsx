5b195affaa1036dbdbecaadac68b1a64ee8c2a6e
Ntuthuko99-NS <at3rdbornzn@gmail.com>
2026-04-13 15:09:14 +0200

    Add ContextMenu component with edit and delete options

---
+++ Components/ui/context-menu.jsx
@@ -0,0 +1,10 @@
+<ContextMenu
+  menuItems={[
+    { label: "Edit", onClick: () => alert("Edit") },
+    { label: "Delete", onClick: () => alert("Delete") },
+  ]}
+>
+  <div style={{ padding: 50 }}>
+    Right-click me
+  </div>
+</ContextMenu>
